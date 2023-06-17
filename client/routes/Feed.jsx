c02793f5cb5dcd88c88153322595202c1de371f1
Alex Vranas <avranas42@gmail.com>
2023-06-17 14:57:49 -0400

    Complete front end

--- client/routes/Feed.jsx
+++ client/routes/Feed.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react';
 import data from '../../twextMockData';
 import Twext from './../Twext';
 import { v4 as uuidv4 } from 'uuid';
+import PostTwext from "../PostTwext";
 
 function Feed() {
   const [text, setText] = useState();
@@ -10,7 +11,12 @@ function Feed() {
   useEffect(() => {
     async function getTwexts() {
       //TODO: Switch to backend data
+
+      
       const response = data;
+
+
+
       setTwextList(
         response.map((twext) => {
           return (
@@ -28,6 +34,8 @@ function Feed() {
   }, []);
   return (
     <div id="feed">
+      <h2>Post new Twext</h2>
+      <PostTwext />
       {twextList}
     </div>
   );
--- client/routes/Login.jsx
+++ client/routes/Login.jsx
@@ -2,18 +2,17 @@ import React, { useEffect, useState } from 'react';
 
 function Login(){
 
-  function handleClick() {
-    console.log('Button Clicked!');
-    // interact with the backend in order to determine whether or not the login in was successful
-  }
+  // function handleClick() {
+  //   console.log('Button Clicked!');
+  //   // interact with the backend in order to determine whether or not the login in was successful
+  //   // redirect to /auth
+  // }
 
   return (
     <div className='center'>
       <h1 className="header">Twexter</h1>
       <div className='center'>
-        {/* eventually make this a button that follows a link */}
-        {/* <button className='element' onClick={}>Log In</button> */}
-        <a href="https://github.com/organizations/axolomehsterz/settings/applications/2234692">Log In</a>
+        <a href='/auth'>Log In!!</a>
       </div>
     </div>
   )
